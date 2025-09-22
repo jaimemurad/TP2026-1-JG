472c6cfcad1ff1e15041c8c40657842e930826c8
JaimeG <example@example.com>
2025-09-22 10:54:15 -0600

    comit 7

--- TP20261/Libreria/Libro.cs
+++ TP20261/Libreria/Libro.cs
@@ -1,4 +1,71 @@
-﻿public class Libro 
+﻿//Programa principal
+
+//Instancias libreria
+
+Libreria libreria = new Libreria();
+
+try {
+    int operaciones = int.Parse(Console.ReadLine() ?? "");
+
+    for (int i = 0; i < operaciones; i++) {
+        string[] entrada = (Console.ReadLine() ?? "").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string comando = entrada[0];
+
+        switch (comando) 
+        {
+            case "LIBRO":
+                if (entrada.Length < 4)
+                    throw new ArgumentException("Faltan datos para agregar el libro.");
+                libreria.AgregarLibro(entrada[1], entrada[2], entrada[3]);
+                break;
+            case "CALIFICAR":
+                if (entrada.Length >= 3) 
+                {
+                    if (!int.TryParse(entrada[2], out int nota))
+                        throw new ArgumentException("La nota debe ser un número entero.");
+                    
+                    if (entrada.Length == 3)
+                    {
+                        libreria.CalificarLibro(entrada[1], nota);
+                    }
+                    else
+                    {
+                        // El comentario puede tener espacios
+                        string comentario = string.Join(' ', entrada.Skip(3));
+                        libreria.CalificarLibro(entrada[1], nota, comentario);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Faltan datos para calificar el libro.");
+                }
+                break;
+            case "MEJOR":
+                
+                libreria.MostrarMejorLibro(entrada[1]);
+                break;
+            case "CRITERIO":
+               
+                libreria.CambiarCriterio(entrada[1]);
+                break;
+            default:
+                throw new InvalidOperationException("Comando no valido");
+
+        }
+    }
+
+} 
+
+catch (Exception ex) 
+{
+    Console.WriteLine($"Error: {ex.Message}");
+
+ }
+
+
+
+
+public class Libro 
 {
     //Atributos
     public string Titulo { get; set; }
@@ -125,7 +192,7 @@ public class Libreria
     private List<Libro> libros = new List<Libro>();
     private IRecomendable estrategiaRecomendacion = new RecomendacionPorPromedio();
 
-    private readonly string[] generosFiccion = {"Ciencia Ficción", "Fantasía", "Misterio", "Romance", "Terror","Novela"};
+    private readonly string[] generosFiccion = {"Ciencia Ficción", "Fantasia", "Misterio", "Romance", "Terror","Novela"};
     private readonly string[] generosTecnicos = { "Programacion", "ED", "Termodinamica", "MecanicaFluidos", "Diccionario", "AnalisisNumerico" };
 
     //Metodos de mi libreria
@@ -141,7 +208,7 @@ public class Libreria
         }
     }
 
-    public void CalificarLibro(string titulo, int nota, string comentario) 
+    public void CalificarLibro(string titulo, int nota) 
     {
         Libro libroEncontrado = null;
 
@@ -162,7 +229,27 @@ public class Libreria
         
         else if(libroEncontrado != null) 
         { 
-            libroEncontrado.Calificar(nota,comentario);
+            libroEncontrado.Calificar(nota);
+        }
+    }
+    //Sobrecarga de calificar libro con comentario
+    public void CalificarLibro(string titulo, int nota, string comentario) 
+    {
+        Libro libroEncontrado = null;
+
+        foreach (var libro in libros) //Busco en todos los libros de la libreria
+        {
+            if (libro.Titulo == titulo) {
+                libroEncontrado = libro;
+                break;
+            }
+
+        }
+
+        if (libroEncontrado == null) {
+            throw new KeyNotFoundException("Libro no encontrado en la librería.");
+        } else if (libroEncontrado != null) {
+            libroEncontrado.Calificar(nota, comentario);
         }
     }
 
@@ -186,4 +273,28 @@ public class Libreria
 
     //Mejor libro
 
+    public void MostrarMejorLibro(string genero) 
+    {
+        List<Libro> librosGenero = new List<Libro>();
+
+        foreach (var libro in libros) 
+        {
+            if (libro.Genero == genero) 
+                
+            {
+              librosGenero.Add(libro);
+            }
+        }
+
+        var mejorLibro = estrategiaRecomendacion.ObtenerMejorLibro(librosGenero);
+
+        if (mejorLibro != null) 
+        {
+            Console.WriteLine(mejorLibro.Titulo);
+        } 
+        else 
+        {
+            Console.WriteLine("Ninguno");
+        }
+    }
 }
